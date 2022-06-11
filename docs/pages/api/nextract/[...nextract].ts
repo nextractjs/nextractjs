1d6be25461196b93ff8ef24508be3608e6fbdcd6
Sönke Peters <soenke@soenkep.com>
2022-06-11 19:11:48 +0200

    fix(docs): correct allowedOrigins
    - add https:// to origin

--- docs/pages/api/nextract/[...nextract].ts
+++ docs/pages/api/nextract/[...nextract].ts
@@ -5,5 +5,5 @@ import Nextract from 'nextract'
 export default Nextract({
   database: PrismaDatabase(prisma),
   isDebug: process.env.NODE_ENV !== 'production',
-  allowedOrigins: ['demo.nextractjs.org'],
+  allowedOrigins: ['https://demo.nextractjs.org'],
 })
